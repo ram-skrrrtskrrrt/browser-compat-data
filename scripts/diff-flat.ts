26a156d417006370fe46700c4d478c959caf745c
Claas Augner <caugner@mozilla.com>
2024-11-21 19:52:34 +0100

    enhance(diff-flat): fetch if ref not available

--- scripts/diff-flat.ts
+++ scripts/diff-flat.ts
@@ -570,5 +570,10 @@ if (esMain(import.meta)) {
 
   const { base, head, group, html, mirror } = options;
 
+  exec(`
+    git rev-parse ${base} || git fetch origin ${base} 2>/dev/null || true &
+    git rev-parse ${head} || git fetch origin ${head} 2>/dev/null || true
+  `);
+
   printDiffs(getMergeBase(base, head), head, { group, html, mirror });
 }
